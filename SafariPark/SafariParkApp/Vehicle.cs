a3e19efc4454c9b0b42b40b7d8874c3ffb5c12da
AidenSpartaGlobal <Skwok@spartaglobal.com>
2023-04-26 15:30:36 +0100

    vehicle

---
+++ SafariPark/SafariParkApp/Vehicle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SafariParkApp
+{
+    public class Vehicle
+    {
+        private int _capacity;
+        private int _numPassengers;
+
+        public int NumPassengers {
+            get { return _numPassengers; }
+            set { if (value < 0 && value > _capacity) throw new ArgumentException(); } }
+
+        public int Position { get; set; }
+        public int Speed { get; init; } = 10;
+
+        public string Move()
+        {
+            Position = Speed * 1;
+            return "Moving along";
+        }
+
+        public string Move(int x)
+        {
+            Position = Speed * x;
+            return $"Moving along {x} times";
+        }
+
+        public Vehicle() 
+        { 
+
+        }
+
+        public Vehicle(int capacity, int speed = 10)
+        {
+            _capacity = capacity;
+            Speed = speed;
+        }
+    }
+}
