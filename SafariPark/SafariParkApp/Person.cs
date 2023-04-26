5c32f7f62e4dbad605610ea82845c9768d210398
AidenSpartaGlobal <Skwok@spartaglobal.com>
2023-04-26 12:58:28 +0100

    third update

---
+++ SafariPark/SafariParkApp/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SafariParkApp
+{
+    public class Person
+    {
+        public string FirstName { get; private set; } = "";
+        public string LastName { get; private set; } = "";
+        private int _age;
+        public int Age 
+        {
+            get { return _age; }
+            set 
+            {
+                if(value < 0 || value > 99)
+                {
+                    throw new ArgumentException();
+                }
+                _age = value;          
+            } 
+        }
+
+        public Person(string firstName, string lastName, int age = 0)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+
+        public Person(string firstName)
+        {
+            FirstName = firstName;
+        }
+
+        //public string GetFullname()
+        //{
+        //    return $"{_firstName} {_lastName}";
+        //}
+
+        public string FullName => $"{FirstName} {LastName}";
+    }
+}
--- SafariPark/SafariParkApp/Program.cs
+++ SafariPark/SafariParkApp/Program.cs
@@ -1,10 +1,21 @@
 ﻿namespace SafariParkApp
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Person joe = new("Joe", "McCann", 40);
+            Console.WriteLine(joe.FullName);
+
+            Person nish = new Person("Nish", "Mandal", 52);
+            Console.WriteLine(nish.FullName);
+
+            Person martin = new Person("Martin");
+            Person michael = new Person("Michael", "Flynn");
+            michael.Age = 32;
+            Console.WriteLine(michael.Age);
+            michael.Age = 100;
+            Console.WriteLine($"{michael.FirstName} is Michael's first name");
         }
     }
 }
